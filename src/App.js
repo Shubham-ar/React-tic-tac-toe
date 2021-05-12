a1a35487eb5883ae25c61b7307b655b314325cda
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-05-12 15:01:19 +0530

    commit on main

--- src/App.js
+++ src/App.js
@@ -21,44 +21,44 @@ const App = () => {
     itemArray.fill("empty", 0, 9)
   }
   const checkIsWinner = () => {
-    if (itemArray[0] == itemArray[1] &&
-      itemArray[0] == itemArray[2] &&
-      itemArray[0] != "empty") {
+    if (itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty") {
       setWinMessage(`${itemArray[0]} wins`);
     }
-    else if (itemArray[3] == itemArray[4] &&
-      itemArray[3] == itemArray[5] &&
-      itemArray[3] != "empty") {
+    else if (itemArray[3] === itemArray[4] &&
+      itemArray[3] === itemArray[5] &&
+      itemArray[3] !== "empty") {
       setWinMessage(`${itemArray[3]} wins`);
     }
-    else if (itemArray[6] == itemArray[7] &&
-      itemArray[6] == itemArray[8] &&
-      itemArray[6] != "empty") {
+    else if (itemArray[6] === itemArray[7] &&
+      itemArray[6] === itemArray[8] &&
+      itemArray[6] !== "empty") {
       setWinMessage(`${itemArray[6]} wins`);
     }
-    else if (itemArray[0] == itemArray[3] &&
-      itemArray[0] == itemArray[6] &&
-      itemArray[0] != "empty") {
+    else if (itemArray[0] === itemArray[3] &&
+      itemArray[0] === itemArray[6] &&
+      itemArray[0] !== "empty") {
       setWinMessage(`${itemArray[0]} wins`);
     }
-    else if (itemArray[1] == itemArray[4] &&
-      itemArray[1] == itemArray[7] &&
-      itemArray[1] != "empty") {
+    else if (itemArray[1] === itemArray[4] &&
+      itemArray[1] === itemArray[7] &&
+      itemArray[1] !== "empty") {
       setWinMessage(`${itemArray[1]} wins`);
     }
-    else if (itemArray[2] == itemArray[5] &&
-      itemArray[2] == itemArray[8] &&
-      itemArray[2] != "empty") {
+    else if (itemArray[2] === itemArray[5] &&
+      itemArray[2] === itemArray[8] &&
+      itemArray[2] !== "empty") {
       setWinMessage(`${itemArray[2]} wins`);
     }
-    else if (itemArray[0] == itemArray[4] &&
-      itemArray[0] == itemArray[8] &&
-      itemArray[0] != "empty") {
+    else if (itemArray[0] === itemArray[4] &&
+      itemArray[0] === itemArray[8] &&
+      itemArray[0] !== "empty") {
       setWinMessage(`${itemArray[0]} wins`);
     }
-    else if (itemArray[2] == itemArray[4] &&
-      itemArray[2] == itemArray[6] &&
-      itemArray[2] != "empty") {
+    else if (itemArray[2] === itemArray[4] &&
+      itemArray[2] === itemArray[6] &&
+      itemArray[2] !== "empty") {
       setWinMessage(`${itemArray[2]} wins`);
 
     }
